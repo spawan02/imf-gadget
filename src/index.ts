a3badb83d5682f001e4ecde0640012b519a017de
pawan <spavandsp@gmail.com>
2025-03-14 14:08:41 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+const app = express()
+const PORT = 3000;
+
+dotenv.config()
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+    res.send("Hello here")
+})
+
+app.listen(PORT,()=>{
+    console.log("Server is listening on port 3000")
+})
